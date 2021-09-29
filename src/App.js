365b2751061426aa0e35af5b3dd2a1d565889735
javidVeg <javidx24@icloud.com>
2021-09-28 21:13:03 -0400

    added App js File

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import DisplaySongs from './DisplaySongs'
 
-function App() {
-  return (
+class App extends Component{
+  constructor(){
+    super();
+    this.state = {
+      songs: []
+    }
+  }
 
-  );
+  componentDidmount(){
+    axios.get('www.devcodecampmusiclibrary.com/api/music')
+    .then(response => this.setState({songs:response.data}));
+  }
+
+  render(){
+    console.log(this.state.songs)
+    return(
+      <div>
+        <DisplaySongs />
+      </div>
+    );
+  }
 }
+
 
 export default App;
